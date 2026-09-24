0e64a8fb3579dede96e04157582aa8e0e382eb7c
Mehmet Yıldız <115743794+mehmetyildiz03@users.noreply.github.com>
2026-09-24 18:47:37 +0300

    Bump PWA cache to V0.40

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='saha112-v039';
-const CORE=['./','./index.html','./styles.css?v=0.39','./cases-data.js?v=0.39','./app-core.js?v=0.39','./manifest.webmanifest?v=0.39','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+const CACHE='saha112-v040';
+const CORE=['./','./index.html','./styles.css?v=0.40','./cases-data.js?v=0.40','./app-core.js?v=0.40','./manifest.webmanifest?v=0.40','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 const NETWORK_FIRST_DESTINATIONS=new Set(['style','script','manifest']);
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
