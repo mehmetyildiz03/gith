407a17e7c8e3727bb2d6ec3578dbcdbc544db646
Mehmet Yıldız <115743794+mehmetyildiz03@users.noreply.github.com>
2026-09-25 23:48:21 +0300

    Bump Saha112 product shell to V0.55

--- cases-data.js
+++ cases-data.js
@@ -1,7 +1,7 @@
 const APP_META = {
   "schemaVersion": 5,
   "contentVersion": "EK2-2026.08.25-early-adult-fidelity-reaudit-2026.09.25",
-  "productVersion": "0.54",
+  "productVersion": "0.55",
   "populations": [
     {
       "id": "adult",
